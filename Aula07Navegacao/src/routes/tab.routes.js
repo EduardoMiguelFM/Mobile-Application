f8b7d7638ff64bcd1df04fa94872dbf106f0632e
edumiguel <dudumfmonteiro@gmail.com>
2025-03-10 11:15:44 -0300

    Login and Home

--- Aula07Navegacao/src/routes/tab.routes.js
+++ Aula07Navegacao/src/routes/tab.routes.js
@@ -1,15 +1,18 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
+import {Feather} from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
 export default function TabRoutes(){
     return(
-        <Tab.Navigator screenOptions={{headerTitleAlign:"center", }}
-        
+        <Tab.Navigator screenOptions={{headerShown:false}}
         >
-            <Tab.Screen  name='home' component={Home}/>
+            <Tab.Screen  name='home' component={Home}
+                options={{tabBarIcon:()=><Feather name='home' size={20} color='red'/>}}
+            />
+
             <Tab.Screen  name='login' component={Login}/>
         </Tab.Navigator>
     )
